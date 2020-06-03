39c49c23b0a22d31f5ea02f909853a0be961295d
raloliver <contato@raloliver.com.br>
2020-06-03 14:16:38 +0100

    Before start models

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const app = express()
-const PORT = 5000
+const mongoose = require('mongoose')
+const PORT = process.env.PORT | 5000
+
+const connectDB = async (operations, res) => {
+    try {
+        const connect = await mongoose.connect(process.env.mongoDB_URI, { 
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        const db = connect.on('connected', () => {
+            console.log('connected on mongo atlas')
+        });
+
+        await operations(db);
+
+        connect.close();
+    } catch (error) {
+        res.status(500).send({ message: 'Error connection to database', error });
+    }
+};
+
 
 const middleware = (req, res, next) => {
     console.log('middleware')
@@ -16,8 +36,13 @@ app.listen(PORT, () => {
     console.warn('Server on 5000')
 })
 
-app.get('/', (req, res) => {
-    res.status(200).send('index')
+app.get('/', async (req, res) => {
+    connectDB(
+        async (db) => {
+            res.status(200).send('index')
+        },
+        res
+    );
 })
 
 app.get('/about', middleware, (req, res) => {
