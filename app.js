1b1cdd420a4a4bc392644a2cef0dcb53abe78eb8
raloliver <contato@raloliver.com.br>
2020-06-03 11:48:03 +0100

    exemple without next()

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 
+const middleware = () => console.log('middleware')
+
+app.use(middleware)
+
 app.listen(PORT, () => {
     console.warn('Server on 5000')
 })
